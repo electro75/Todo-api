4ca0c23753faa80523e21a7b6f3f644e25351b2b
sahil n <sahil@mutterfly.in>
2018-02-13 16:40:01 +0530

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,17 @@
 var mongoose = require('mongoose');
 var moment = require('moment');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+//copy the link from "To connect using a driver via the standard MongoDB URI" section
+//insert db user name and password here
+const REMOTE_MONGO = 'mongodb://sahil7595:12345678@ds233208.mlab.com:33208/todos-api';
+const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
+const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
 
-module.exports = { mongoose }
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI).then(() => {
+    console.log(`Connected to Mongo instance: ${ MONGO_URI }`)
+}, (err) => {
+    console.log('Error connecting to Mongo instance: ', err);
+});
+
+module.export = { mongoose };
