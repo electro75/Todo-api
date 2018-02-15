35da8d12e985d568c236bf66a14534f77383c2ce
sahil n <sahil@mutterfly.in>
2018-02-15 15:57:22 +0530

    test commit

--- server/db/keys.js
+++ server/db/keys.js
@@ -1,4 +1,5 @@
-const REMOTE_MONGO = 'mongodb://sahil7595:12345678@ds233208.mlab.com:33208/todos-api';
+// const REMOTE_MONGO = 'mongodb://sahil7595:12345678@ds233208.mlab.com:33208/todos-api';
+const REMOTE_MONGO = 'mongodb://bata_gang:batagang@ds235778.mlab.com:35778/test_table'
 
 module.exports = {
     REMOTE_MONGO
