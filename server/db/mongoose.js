2aa4fe2a1fafe3728820fb13c72602afae4bafd1
Sahil Naikdhure <sanaikdhure@gmail.com>
2018-02-21 19:39:10 +0530

    updates the app configuration

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var moment = require('moment');
 var { REMOTE_MONGO } = require('./keys');
-// const REMOTE_MONGO = 'mongodb://sahil7595:12345678@ds233208.mlab.com:33208/todos-api';
+// const REMOTE_MONGO = 'mongodb://{username}:{userpass}@ds233208.mlab.com:33208/todos-api';
 
 var env = process.env.NODE_ENV || 'development';
 console.log('env ****', env);
@@ -13,18 +13,19 @@ var db = {
 if(process.env.PORT){
   process.env.MONGODB_URI = db.mlab;
 } else {
-  process.env.PORT = 3000;
-  if(env === 'development'){
-    process.env.MONGODB_URI = db.dev;
-  } else if (env === 'test'){
-    process.env.MONGODB_URI = db.test;
-  }
+  // process.env.PORT = 3000;
+  // if(env === 'development'){
+  //   process.env.MONGODB_URI = db.dev;
+  // } else if (env === 'test'){
+  //   process.env.MONGODB_URI = db.test;
+  // }
+  var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key)=>{
+      process.env[key] = envConfig[key];
+    });
 }
-
-//copy the link from "To connect using a driver via the standard MongoDB URI" section
-//insert db user name and password here
-
-
 
 const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
 
