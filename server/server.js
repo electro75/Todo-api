b8dc51f6c9292822019f4104d42bfb9119234812
sahil n <sahil@mutterfly.in>
2018-10-24 11:43:51 +0530

    adds access-control-expose-headers command

--- server/server.js
+++ server/server.js
@@ -14,6 +14,10 @@ const port = process.env.PORT;
 
 app.use(cors());
 
+app.use(require('access-control')({
+    exposed: ['X-Auth']
+}))
+
 app.use(bodyParser.json());
 
 app.post('/todos', authenticate, (req, res)=>{
